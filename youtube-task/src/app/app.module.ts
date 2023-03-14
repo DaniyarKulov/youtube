f3a52a04d1b4eefa45b154e4b985671f0a385006
Daniyar <kulovdaniyar77@gmail.com>
2023-03-14 19:55:21 +0600

    feat: task intro week 1

--- youtube-task/src/app/app.module.ts
+++ youtube-task/src/app/app.module.ts
@@ -1,31 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
-import { HeaderModule } from './core/header/header.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
-import { YoutubeModule } from './youtube/youtube.module';
-import { SharedModule } from './shared/shared.module';
+import { HeaderModule } from './header/header.module';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
   declarations: [AppComponent],
-  providers: [],
   bootstrap: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HeaderModule,
-    CoreModule,
-    HttpClientModule,
-    YoutubeModule,
-    MatSlideToggleModule,
-    MatButtonModule,
-    BrowserAnimationsModule,
-    SharedModule,
-  ],
+  imports: [AppRoutingModule, HeaderModule, SearchModule],
 })
 export class AppModule {}
---
+++ youtube-task/src/app/search/search-item.model.ts
@@ -0,0 +1,41 @@
+export interface SearchItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  statistics: Statistics;
+}
+
+export interface Snippet {
+  publishedAt: Date;
+  channelId: string;
+  title: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+}
+
+export interface Thubnails {
+  default: Thubnail;
+  medium: Thubnail;
+  high: Thubnail;
+  standard: Thubnail;
+  maxres: Thubnail;
+}
+export interface Localized {
+  title: string;
+  description: string;
+}
+export interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  commentCount: string;
+}
+
+export interface Thubnail {
+  url: string;
+  width: number;
+  height: number;
+}
