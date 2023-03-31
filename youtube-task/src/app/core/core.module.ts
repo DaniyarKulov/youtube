9376e071cb8b575c40f77047b3fa1ddca011a216
DaniyarKulov <kulovdaniyar77@gmail.com>
2023-04-01 01:38:01 +0600

    refactor: export headerModule

--- youtube-task/src/app/core/core.module.ts
+++ youtube-task/src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderModule } from './header/header.module';
-import { HeaderComponent } from './header/components/header/header.component';
 
 @NgModule({
   imports: [CommonModule, HeaderModule],
-  exports: [HeaderComponent],
+  exports: [HeaderModule],
 })
 export class CoreModule {}
